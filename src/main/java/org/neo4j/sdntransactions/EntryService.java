c33123e5f1d1ef27740caa90297480233baf3c2a
Gerrit Meier <meistermeier@gmail.com>
2025-07-01 07:38:29 +0200

    use driver transactions explicitly

--- src/main/java/org/neo4j/sdntransactions/EntryService.java
+++ src/main/java/org/neo4j/sdntransactions/EntryService.java
@@ -2,6 +2,7 @@ package org.neo4j.sdntransactions;
 
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.SessionConfig;
+import org.neo4j.driver.TransactionConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
 /**
  * @author Gerrit Meier
@@ -19,11 +21,13 @@ public class EntryService {
     private final EntryRepository repository;
     private final Driver driver;
     private final SessionConfig sessionConfig;
+    private final TransactionConfig transactionConfig;
 
     public EntryService(EntryRepository repository, Driver driver, @Value("${spring.data.neo4j.database}") String databaseName) {
         this.repository = repository;
         this.driver = driver;
         this.sessionConfig = SessionConfig.builder().withDatabase(databaseName).build();
+        this.transactionConfig = TransactionConfig.builder().withMetadata(Map.of("static", "metadata")).build();
     }
 
     @Transactional
@@ -33,12 +37,14 @@ public class EntryService {
 
     public String returnNamesViaDriver() {
         long start = System.currentTimeMillis();
-        try (var session = driver.session(sessionConfig)) {
+        try (var session = driver.session(sessionConfig);
+             var transaction = session.beginTransaction(transactionConfig)) {
 
             var names = new ArrayList<String>();
-            session.run("MATCH (e:Entry) RETURN e.name").forEachRemaining(record ->
+            transaction.run("MATCH (e:Entry) RETURN e.name").forEachRemaining(record ->
                 names.add(record.get("e.name").asString())
             );
+            transaction.commit();
             long end = System.currentTimeMillis();
             System.out.println("Driver write: " + Duration.ofMillis(end - start));
             return String.join(",", names);
--- src/main/java/org/neo4j/sdntransactions/EntryServiceReadonly.java
+++ src/main/java/org/neo4j/sdntransactions/EntryServiceReadonly.java
@@ -3,6 +3,7 @@ package org.neo4j.sdntransactions;
 import org.neo4j.driver.AccessMode;
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.SessionConfig;
+import org.neo4j.driver.TransactionConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
 /**
  * @author Gerrit Meier
@@ -20,11 +22,13 @@ public class EntryServiceReadonly {
     private final EntryRepositoryReadOnly repository;
     private final Driver driver;
     private final SessionConfig sessionConfig;
+    private final TransactionConfig transactionConfig;
 
     public EntryServiceReadonly(EntryRepositoryReadOnly repository, Driver driver,  @Value("${spring.data.neo4j.database}") String databaseName) {
         this.repository = repository;
         this.driver = driver;
         this.sessionConfig = SessionConfig.builder().withDatabase(databaseName).withDefaultAccessMode(AccessMode.READ).build();
+        this.transactionConfig = TransactionConfig.builder().withMetadata(Map.of("static", "metadata")).build();
     }
 
     @Transactional(readOnly = true)
@@ -34,14 +38,16 @@ public class EntryServiceReadonly {
 
     public String returnNamesViaDriver() {
         long start = System.currentTimeMillis();
-        try (var session = driver.session(sessionConfig)) {
+        try (var session = driver.session(sessionConfig);
+             var transaction = session.beginTransaction(transactionConfig)) {
 
             var names = new ArrayList<String>();
-            session.run("MATCH (e:Entry) RETURN e.name").forEachRemaining(record ->
+            transaction.run("MATCH (e:Entry) RETURN e.name").forEachRemaining(record ->
                 names.add(record.get("e.name").asString())
             );
+            transaction.commit();
             long end = System.currentTimeMillis();
-            System.out.println("Driver readonly: " + Duration.ofMillis(end - start));
+            System.out.println("Driver write: " + Duration.ofMillis(end - start));
             return String.join(",", names);
         }
     }
